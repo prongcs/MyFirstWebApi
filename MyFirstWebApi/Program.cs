bbb65e3c1babb41689e06063a06295b79530b5c2
rkaligat <rkaligat@starbucks.com>
2018-07-17 14:44:41 -0700

    configured App Insights

--- MyFirstWebApi/Program.cs
+++ MyFirstWebApi/Program.cs
@@ -20,6 +20,7 @@ namespace MyFirstWebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .UseNLog();
     }
